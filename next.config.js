d27107d84d84cbebe5ccba407edb160369e6a0ec
Alejandro Gonzalez Alonso <alegonzo97@gmail.com>
2021-05-25 14:46:23 -0400

    fix api url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
     },
     basePath: process.env.BASE_PATH || '/wiroforce'
 }
