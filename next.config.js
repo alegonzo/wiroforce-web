af7076478f51387bdf0a89d06020e74bc3cbf485
Alejandro Gonzalez Alonso <alegonzo97@gmail.com>
2021-05-28 13:04:43 -0400

    test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     env: {
-        NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
+        NEXTAUTH_URL: process.env.NEXTAUTH_URL
     },
     basePath: process.env.BASE_PATH || '/wiroforce'
 }
--- utils/api.ts
+++ utils/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const AxiosHelper = () => {
+    console.log(process.env.NEXT_PUBLIC_API_URL);
     return axios.create({
         baseURL: `${process.env.NEXT_PUBLIC_API_URL}`
     });
