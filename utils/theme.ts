46a581719454f61743047c4c0ddb8d093916464d
Alejandro Gonzalez Alonso <alegonzo97@gmail.com>
2021-05-29 13:13:20 -0400

    fix color

--- utils/theme.ts
+++ utils/theme.ts
@@ -12,7 +12,7 @@ const theme = createMuiTheme({
             main: '#186AFA'
         },
         background: {
-            default: '#21212E'
+            default: '#2e353b'
         }
     },
 });
