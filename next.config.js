8ee97856944d3782266e3ad5d9463377e25c8f8c
Alejandro Gonzalez Alonso <alegonzo97@gmail.com>
2021-06-20 13:29:06 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     env: {
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+        NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
     },
     publicRuntimeConfig: {
         // Will be available on both server and client
