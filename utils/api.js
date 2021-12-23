77c05c445c0c1c28e9dbbd0473c199226f71d8c2
Alejandro Gonzalez Alonso <alegonzo97@gmail.com>
2021-12-23 12:56:59 -0500

    fix

--- utils/api.js
+++ utils/api.js
@@ -5,15 +5,19 @@ import { getSession } from 'next-auth/client'
 
 export const api = async (url, options = { method: 'get', params: {} }) => {
   const session = await getSession()
-
-  const { headers, ...config } = options
-  const response = await axios(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
-    headers: Object.assign(headers || {}, {
-      Authorization: `Bearer ${session.user.token}`,
-    }),
-    ...config,
-  })
-  return response
+  try {
+    const { headers, ...config } = options
+    const response = await axios(`${process.env.NEXT_PUBLIC_API_URL}${url}`, {
+      headers: Object.assign(headers || {}, {
+        Authorization: `Bearer ${session.user.token}`,
+      }),
+      ...config,
+    })
+    return response
+  } catch (e) {
+    if (e?.response?.status === 401) await signOut()
+    throw e
+  }
 }
 
 /*
